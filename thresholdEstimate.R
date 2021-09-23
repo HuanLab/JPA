cf61d63cb2c1c3f9650d6ab6486dfa6d999cd29a
HuanLab <52514403+HuanLab@users.noreply.github.com>
2021-09-23 10:03:10 -0700

    Update thresholdEstimate.R

--- thresholdEstimate.R
+++ thresholdEstimate.R
@@ -1,3 +1,47 @@
+#noise average and sd estimation#
+directory <- "F:/Jian_Guo"
+setwd(directory)
+filename <- list.files(pattern = ".mzXML")
+ms1data <- readMSData(files = filename[q], mode = "onDisk", msLevel. = 1)
+mzData <- mz(ms1data)
+intData <- intensity(ms1data)
+currmzRange <- c(mzmin, mzmax)
+    tmpMZdata <- mzData
+    tmpINTdata <- intData
+    for(j in 1:length(mzData)){
+      index <- which(tmpMZdata[[j]] >= currmzRange[1] & tmpMZdata[[j]] < currmzRange[2])
+      tmpMZdata[[j]] <- tmpMZdata[[j]][index]
+      tmpINTdata[[j]] <- tmpINTdata[[j]][index]
+    }
+    
+    eicINTraw <- c()
+    eicINT <- c()
+    eicRT <- c()
+    for(k in 1:length(mzData)){
+      if(length(tmpINTdata[[k]]) > 0){
+        eicINTraw[k] <- mean(tmpINTdata[[k]])
+      }else{
+        eicINTraw[k] <- 0
+      }
+      eicRT[k] <- rtime[k]
+    }
+    if(sum(eicINTraw != 0) == 0) next()
+    eicINT <- peak_smooth(eicINTraw)
+    eicNon0 <- sort(eicINT[eicINT > 0])
+    if(length(eicNon0) > 10){
+      for(x in seq(10,length(eicNon0), 10)){
+        sd <- sd(eicNon0[1:x])
+        blk <- sum(eicNon0[1:x])/x
+        thres <- blk + 3*sd
+        if(x+1 <= length(eicNon0)){
+          if(eicNon0[x+1] >= thres) break()
+        }
+      }
+    }
+signal <- blk
+variance <- sd
+
+
 #the noise threshold determination model#
 hz <- 5
 signal <- 100
