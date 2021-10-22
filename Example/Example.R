1e84108aaf1c53ba06dba4350b67fd8e13c0b782
HuanLab <52514403+HuanLab@users.noreply.github.com>
2021-10-21 17:25:54 -0700

    Update and rename test.R to Example.R

--- Example/Example.R
+++ Example/Example.R
@@ -3,64 +3,82 @@ library(IPA)
 #########################################################################################
 # Workflow 1: XCMS -> level3 -> alignment -> plot -> annotation
 # Single
-dir = "X:/Users/Sam_Shen/IPAtest_20210330/singleDDA"
+dir = "X:/Users/IPAtest_20210330/singleDDA"
+# Extract JPA-PP fetures 
 featureTable <- XCMS.featureTable(dir = dir, mz.tol = 10, ppm=10, peakwidth=c(5,20), mzdiff = 0.01,
                              snthresh = 6, integrate = 1, prefilter = c(3,100), noise = 100)
+# Extract JPA-MR fetures
 featureTable <- find.level3features(data = MSdata)
 
 # Multi
-dir = "X:/Users/Sam_Shen/IPAtest_20210330/multiDDA"
+dir = "X:/Users/IPAtest_20210330/multiDDA"
+# Extract JPA-PP fetures
 featureTable <- XCMS.featureTable(dir = dir, mz.tol = 10, ppm=10, peakwidth=c(5,20), mzdiff = 0.01,
                              snthresh = 6, integrate = 1, prefilter = c(3,100), noise = 100)
+# Extract JPA-MR fetures
 featureTable <- find.level3features(data = MSdata)
 featureTable <- peak.alignment(data = MSdata, bw = 5, minfrac = 0.5, mzwid = 0.015,
                                minsamp = 1, max = 100, quantitative.method = "maxo")
 #########################################################################################
 # Workflow 2: XCMS -> level3 -> target -> alignment -> plot -> annotation
 # Single
-dir = "X:/Users/Sam_Shen/IPAtest_20210330/singleDDA"
-tarFTdir = "X:/Users/Sam_Shen/IPAtest_20210330"
+dir = "X:/Users/IPAtest_20210330/singleDDA"
+tarFTdir = "X:/Users/IPAtest_20210330"
 tarFTname = "LibraryCSVHILIC-.csv"
+# Extract JPA-PP fetures
 featureTable <- XCMS.featureTable(dir = dir, mz.tol = 10, ppm=10, peakwidth=c(5,20), mzdiff = 0.01,
                              snthresh = 6, integrate = 1, prefilter = c(3,100), noise = 100)
+# Extract JPA-MR fetures                             
 featureTable <- find.level3features(data = MSdata)
+# Extract JPA-TL fetures
 featureTable <- find.targetfeatures(data = MSdata, tarFTdir = tarFTdir, tarFTname = tarFTname)
 
 # Multi
 dir = "X:/Users/Sam_Shen/IPAtest_20210330/multiDDA"
-tarFTdir = "X:/Users/Sam_Shen/IPAtest_20210330"
+tarFTdir = "X:/Users/IPAtest_20210330"
 tarFTname = "LibraryCSVHILIC-.csv"
+# Extract JPA-PP fetures
 featureTable <- XCMS.featureTable(dir = dir, mz.tol = 10, ppm=10, peakwidth=c(5,20), mzdiff = 0.01,
                              snthresh = 6, integrate = 1, prefilter = c(3,100), noise = 100)
+# Extract JPA-MR fetures
 featureTable <- find.level3features(data = MSdata)
+# Extract JPA-TL fetures
 featureTable <- find.targetfeatures(data = MSdata, tarFTdir = tarFTdir, tarFTname = tarFTname)
+# Deisotope the features
 featureTable <- adduct.isotope.annotation(data = MSdata, polarity = "negative")
 featureTable <- peak.alignment(data = MSdata, bw = 5, minfrac = 0.5, mzwid = 0.015,
                                minsamp = 1, max = 100, quantitative.method = "maxo")
 #########################################################################################
 # Workflow 3: Existing Feature table -> level3 -> alignment -> plot -> annotation
 # Single
-dir = "X:/Users/Sam_Shen/IPAtest_20210330/singleDDA"
-FTdir = "X:/Users/Sam_Shen/IPAtest_20210330/singleDDA"
+dir = "X:/Users/IPAtest_20210330/singleDDA"
+FTdir = "X:/Users/IPAtest_20210330/singleDDA"
+# Input JPA-PP fetures from other software
 featureTable <- custom.featureTable(dir = dir, FTdir = FTdir)
+# Extract JPA-MR fetures
 featureTable <- find.level3features(data = MSdata)
 
 # Multi
-dir = "X:/Users/Sam_Shen/IPAtest_20210330/multiDDA"
-FTdir = "X:/Users/Sam_Shen/IPAtest_20210330/multiDDA"
+dir = "X:/Users/IPAtest_20210330/multiDDA"
+FTdir = "X:/Users/IPAtest_20210330/multiDDA"
+# Input JPA-PP fetures from other software
 featureTable <- custom.featureTable(dir = dir, FTdir = FTdir)
+# Extract JPA-MR fetures
 featureTable <- find.level3features(data = MSdata)
 featureTable <- peak.alignment(data = MSdata, bw = 5, minfrac = 0.5, mzwid = 0.015,
                                minsamp = 1, max = 100, quantitative.method = "maxo")
 #########################################################################################
 # Workflow 4: Existing Feature table -> level3 -> target -> alignment -> plot -> annotation
 # Single
-dir = "X:/Users/Sam_Shen/IPAtest_20210330/singleDDA"
-FTdir = "X:/Users/Sam_Shen/IPAtest_20210330/singleDDA"
+dir = "X:/Users/IPAtest_20210330/singleDDA"
+FTdir = "X:/Users/IPAtest_20210330/singleDDA"
 tarFTdir = "X:/Users/Sam_Shen/IPAtest_20210330"
 tarFTname = "LibraryCSVHILIC-.csv"
+# Input JPA-PP fetures from other software
 featureTable <- custom.featureTable(dir = dir, FTdir = FTdir)
+# Extract JPA-MR fetures
 featureTable <- find.level3features(data = MSdata)
+# Extract JPA-TL fetures
 featureTable <- find.targetfeatures(data = MSdata, tarFTdir = tarFTdir, tarFTname = tarFTname)
 
 # Multi
@@ -68,8 +86,11 @@ dir = "X:/Users/Sam_Shen/IPAtest_20210330/multiDDA"
 FTdir = "X:/Users/Sam_Shen/IPAtest_20210330/multiDDA"
 tarFTdir = "X:/Users/Sam_Shen/IPAtest_20210330"
 tarFTname = "LibraryCSVHILIC-.csv"
+# Input JPA-PP fetures from other software
 featureTable <- custom.featureTable(dir = dir, FTdir = FTdir)
+# Extract JPA-MR fetures
 featureTable <- find.level3features(data = MSdata)
+# Extract JPA-TL fetures
 featureTable <- find.targetfeatures(data = MSdata, tarFTdir = tarFTdir, tarFTname = tarFTname)
 featureTable <- peak.alignment(data = MSdata, bw = 5, minfrac = 0.5, mzwid = 0.015,
                                minsamp = 1, max = 100, quantitative.method = "maxo")
